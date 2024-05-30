053c6ef9dc109e9d7f0bc1f542fefcc837df2d77
tostu <torge.stubbe@googlemail.com>
2024-05-30 21:15:02 +0200

    edit Layout.svelte

--- src/lib/layout/Layout.svelte
+++ src/lib/layout/Layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import "./layout.css"
-
+	import "../global.css"
 	export let hideFooter = false;
 </script>
 
 <div class="flex min-h-svh flex-col">
 	<slot name="header"/>
 	<div class="flex-grow">
-		<slot name="main"/>
+		<slot name="main" class="flex flex-col"/>
 		{#if !hideFooter}
 			<slot name="footer"/>
 		{/if}
